756c184d01134d89b8c2278e56a92fd361b799b7
Ye Htet Aung <yehtet804p@gmail.com>
2023-08-12 13:44:44 +0630

    fourth commit

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     env:{
-        MONGOL_URL:"",
-        JWT_KEY:"",
+        MONGOL_URL:"mongodb+srv://yehtetaung:446646804@cluster-note.p5gmpex.mongodb.net/?retryWrites=true&w=majority",
+        JWT_KEY:"dafjfalkfjoadshfjahfafasdf",
     },
 }
